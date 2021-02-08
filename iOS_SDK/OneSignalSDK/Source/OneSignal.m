9e013b82224341dfcee31c97182f7e97296ffbe3
emawby <emawby@gmail.com>
2021-02-08 14:56:53 -0800

    testing provisional

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -1662,6 +1662,7 @@ static dispatch_queue_t serialQueue;
     let notificationTypes = [self getNotificationTypes];
     mLastNotificationTypes = notificationTypes;
     userState.notificationTypes = [NSNumber numberWithInt:notificationTypes];
+    NSLog(@"ECM user state notification types %@", userState.notificationTypes);
     
     let CTTelephonyNetworkInfoClass = NSClassFromString(@"CTTelephonyNetworkInfo");
     if (CTTelephonyNetworkInfoClass) {
@@ -1776,6 +1777,7 @@ static dispatch_queue_t serialQueue;
         }
         
         if (results[@"push"][@"in_app_messages"]) {
+            NSLog(@"ECM received in app");
             [self receivedInAppMessageJson:results[@"push"][@"in_app_messages"]];
         }
     } onFailure:^(NSDictionary<NSString *, NSError *> *errors) {
@@ -2080,7 +2082,6 @@ static NSString *_lastnonActiveMessageId;
 
 + (int)getNotificationTypes {
     [OneSignal onesignal_Log:ONE_S_LL_VERBOSE message: [NSString stringWithFormat:@"getNotificationTypes:mSubscriptionStatus: %d", mSubscriptionStatus]];
-    
     if (mSubscriptionStatus < -9)
         return mSubscriptionStatus;
     
@@ -2088,6 +2089,8 @@ static NSString *_lastnonActiveMessageId;
         return ERROR_PUSH_DELEGATE_NEVER_FIRED;
     
     OSPermissionState* permissionStatus = [self.osNotificationSettings getNotificationPermissionState];
+    
+    NSLog(@"ECM getnotiftypes %i", permissionStatus.notificationTypes);
     
     //only return the error statuses if not provisional
     if (!permissionStatus.provisional && !permissionStatus.hasPrompted)
