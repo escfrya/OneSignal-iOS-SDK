8518f9cbb724f914f3099686372d9352cc3961b9
emawby <emawby@gmail.com>
2021-02-10 16:28:09 -0800

    making cached tags work with user state

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -1194,6 +1194,7 @@ void onesignal_Log(ONE_S_LOG_LEVEL logLevel, NSString* message) {
     
     // Can't send tags yet as their isn't a player_id.
     //   tagsToSend will be sent with the POST create player call later in this case.
+    NSLog(@"ECM add tags but not sending to server");
     [self.playerTags addTags:tagsToSend];
     if (self.currentSubscriptionState.userId)
        [OneSignalHelper performSelector:@selector(sendTagsToServer) onMainThreadOnObject:self withObject:nil afterDelay:5];
@@ -1724,6 +1725,8 @@ static dispatch_queue_t serialQueue;
     NSArray* nowProcessingCallbacks;
     let userState = [self createUserState];
     if (userState.tags) {
+        [self.playerTags addTags:userState.tags];
+        [OneSignal.getPlayerTags saveTagsToUserDefaults];
         tagsToSend = nil;
         
         nowProcessingCallbacks = pendingSendTagCallbacks;
@@ -1764,7 +1767,6 @@ static dispatch_queue_t serialQueue;
             }
             
             if (tagsToSend) {
-                [self.playerTags addTags:tagsToSend];
                 [self performSelector:@selector(sendTagsToServer) withObject:nil afterDelay:5];
             }
                 
@@ -2681,6 +2683,7 @@ static NSString *_lastnonActiveMessageId;
 
     return true;
 }
+
 @end
 
 @implementation OneSignal (SessionStatusDelegate)
